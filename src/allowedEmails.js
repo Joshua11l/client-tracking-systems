78fc141f7c8920a7319e9a62df66a7e3fc4adcb2
Joshua Lopez <jlopez0627@csu.fullerton.edu>
2024-06-20 14:08:37 -0700

    new emails

--- src/allowedEmails.js
+++ src/allowedEmails.js
@@ -1,6 +1,11 @@
-// src/allowemails.js
+// src/allowedemails.js
 export const allowedUsers = [
   { email: "divinesoftwaresystems@gmail.com", name: "Joshua Lopez", position: "Co-Founder" },
-  { email: "allowedemail2@example.com", name: "User Two" },
+  { email: "jrodriguez@divinesoftwaresystems.com", name: "Justin Rodriguez", position: "Co-Founder" },
+  { email: "syonchau@gmail.com", name: "Syon Chau", position: "Lead Web Developer" },
+  { email: "achang@divinesoftwaresystems.com", name: "Alex Hong", position: "Software Engineer" },
+  { email: "evanesmiller@gmail.com", name: "Evan Miller", position: "Software Engineer" },
+  { email: "adrian.dss.b@gmail.com", name: "Adrian Bautista", position: "Software Engineer" },
+
   // Add more allowed users here
 ];
