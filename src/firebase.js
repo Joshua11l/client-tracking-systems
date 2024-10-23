c7975c8a5ada7da701a678c1ad71bc84dbbe2a70
Joshua Lopez <112032410+Joshua11l@users.noreply.github.com>
2024-10-23 12:30:31 -0700

    fixed api error

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage'; // Import the storage module
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7nA9z-SixohZYEwGHCcakiYQKAMsMo9E",
-  authDomain: "client-tracker-29433.firebaseapp.com",
-  projectId: "client-tracker-29433",
-  storageBucket: "client-tracker-29433.appspot.com",
-  messagingSenderId: "499927985086",
-  appId: "1:499927985086:web:9c6ba816dabd03afc367ef",
-  measurementId: "G-QN152WQCJ6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const firestore = getFirestore(app);
-export const storage = getStorage(app); // Initialize Firebase Storage
+export const storage = getStorage(app);
