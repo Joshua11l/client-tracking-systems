b0dc7272692addb57de3e459fecd868f26464522
Joshua Lopez <112032410+Joshua11l@users.noreply.github.com>
2024-10-23 12:13:37 -0700

    Fixed project link not displaying clien t error

--- generateLinks.js
+++
@@ -1,8 +0,0 @@
-// generateLinks.js
-const clients = require('./src/clients.json');
-
-const baseUrl = 'https://your-vercel-domain.com/client';
-
-clients.clients.forEach(client => {
-  console.log(`${client.name}: ${baseUrl}/${client.id}`);
-});
